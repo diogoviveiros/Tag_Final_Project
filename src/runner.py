dc9347d4fba91c3c7e488708f65e8622e707c9d9
diogoviveiros <diogopviveiros@gmail.com>
2022-05-25 00:30:25 -0500

    Runner will now avoid walls and frontal collisions

--- src/runner.py
+++ src/runner.py
@@ -6,6 +6,7 @@ import random
 
 # msgs needed for /cmd_vel
 from geometry_msgs.msg import Twist, Vector3
+from sensor_msgs.msg import LaserScan
 
 class Runner(object):
 
@@ -14,6 +15,8 @@ class Runner(object):
         rospy.init_node('runner')
         # setup publisher to the cmd_vel ROS topic
         self.robot_movement_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        #Setup publisher for laser scanning
+        self.laser_sub = rospy.Subscriber("/scan", LaserScan, self.process_scan, queue_size =1)
     
     def run(self):
 
@@ -36,7 +39,24 @@ class Runner(object):
 
         rospy.sleep(random.randrange(4,9))
 
+    def process_scan(self, data):
 
+        min_dist = 5
+        
+        #Checks the front of the robot for an obstacle (particularly walls)
+        if(data.ranges[0] <= min_dist and data.ranges[0] != 0.0):
+            angle = random.uniform(-4.71, 4.71)
+            while(math.abs(angle) < 4.71): 
+                angle = random.uniform(-4.71, 4.71)
+
+            self.twist.angular.z = angle
+
+         
+        #Rotate in a random direction predetermined in the if condition right above
+    
+            
+        self.twist_pub.publish(self.twist)
+        rospy.delay(2)
 
 if __name__ == '__main__':
     # instantiate the ROS node and run it
